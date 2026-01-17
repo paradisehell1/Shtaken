03a7877aba5fb93f9941b0e5790e90727ae33038
paradisehell1 <chelovek228339@mail.ru>
2026-01-17 17:32:08 +0300

    sss

--- ShtakenShneider/shtaken/urls.py
+++ ShtakenShneider/shtaken/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
-from . import views
+# ShtakenShneider/urls.py
+from django.contrib import admin
+from django.urls import path, re_path
+from shtaken import views
 
 urlpatterns = [
-    path('', views.booking, name='booking'),
+    path('', views.booking, name='booking'),               # главная
+    re_path(r'^miniApp/?$', views.miniApp, name='miniApp'), # optional slash
     path('api/bookings/', views.get_bookings, name='get_bookings'),
-    path('miniApp/', views.miniApp, name='miniApp'),
+    path('admin/', admin.site.urls),
 ]
