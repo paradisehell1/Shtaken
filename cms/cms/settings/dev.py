95b0866daf1c57cd690823079b453a4816491e87
paradisehell1 <chelovek228339@mail.ru>
2026-01-22 11:12:59 +0300

    start admin

---
+++ cms/cms/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-_ljou2-%7!jzqd-92!c4ylg#o-jd8@_%@rk&i%8ugrsgl7olj!"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
